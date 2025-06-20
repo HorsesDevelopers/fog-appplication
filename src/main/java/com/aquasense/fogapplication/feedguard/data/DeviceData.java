a0ef4d66ce38730131d6c8b7aa8673037bc476e3
Diego Flores <u20221b394@upc.edu.pe>
2025-06-20 11:56:46 -0500

    feat(fog): advance.

---
+++ src/main/java/com/aquasense/fogapplication/feedguard/data/DeviceData.java
@@ -0,0 +1,26 @@
+package com.aquasense.fogapplication.feedguard.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class DeviceData {
+
+    private String deviceId;
+
+    private String type;
+
+    private Float value;
+
+    private String status;
+
+    DeviceData(String deviceId, String type, Float value, String status) {
+        this.deviceId = deviceId;
+        this.type = type;
+        this.value = value;
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/aquasense/fogapplication/feedguard/interfaces/PondDeviceController.java
@@ -0,0 +1,13 @@
+package com.aquasense.fogapplication.feedguard.interfaces;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fog1/v1/ponds")
+public class PondDeviceController {
+
+    //@Autowired
+    //
+}
