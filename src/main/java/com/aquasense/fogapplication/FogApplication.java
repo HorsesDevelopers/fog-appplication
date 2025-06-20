c883af9356ca03ec1d02686b3743944b9578f723
Diego Flores <u20221b394@upc.edu.pe>
2025-06-20 11:18:57 -0500

    chore: initial commit.

---
+++ src/main/java/com/aquasense/fogapplication/FogApplication.java
@@ -0,0 +1,13 @@
+package com.aquasense.fogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aquasense/fogapplication/FogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquasense.fogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
